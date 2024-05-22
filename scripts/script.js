ebdc0978d7143d12288a5ba37229ef1d0271e72d
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-05-22 18:07:53 +0300

    0.1.1

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,13 @@ const widthMediaQuery = window.matchMedia(
 	'(width >= 768px) and (width < 1102px)'
 )
 const widthMediaQueryDesktop = window.matchMedia('(min-width: 1102px)')
-const swiper = document.querySelector('.swiper')
 const readMore = document.querySelector('.read-more')
 const readMoreButton = readMore.querySelector('.read-more__button')
 const swiperSlide = document.querySelectorAll('.swiper-slide')
 const hiddenClass = document.querySelector('.hidden')
 
 function swiperInit() {
-	const swiper = new Swiper('.swiper', {
+	new Swiper('.swiper', {
 		breakpoints: {
 			0: {
 				enabled: true,
